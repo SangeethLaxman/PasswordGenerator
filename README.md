f8b757a58b7ec95cf4630cc14fdb71daaef35a01
Mythic4356 <152592936+SangeethLaxman@users.noreply.github.com>
2025-06-25 19:43:49 +0400

    dark/light mode and clipboard

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Password Generator
+
+A simple yet efficient password generator that provides completley random and secure passwords, with the capability of testing your own handmade passwords and even adding words to the password for easy memorization!
+
+## Requirements
+- =< Python 3.12.4
+- customtkinter library
+- colour library
+- zxcvbn library
--- main.py
+++ main.py
@@ -9,14 +9,13 @@ import string
 
 import time # Time library to calculate time spent on generating password (debugging)
 from customtkinter import * #Custom Tkinter Library for better UI
-os.system("pip install zxcvbn colour") #Install the zxcvbn and colour libraries
 import colour #Colour library for color manipulation and processing
 import zxcvbn #ZXCVBN Password Strength Estimation Library by Dropbox
 
 
 
 set_default_color_theme("green")
-set_appearance_mode("light")
+set_appearance_mode("dark")
 
 #Obtain list of characters
 spchar = ["$","%","&","!","?","@","#","_","-","+","="]
@@ -36,7 +35,7 @@ strengths=[
 #init root
 root = CTk()
 root.title("Password Generator")
-root.geometry("770x330")
+root.geometry("710x485")
 root.resizable(False,False)
 
 
@@ -56,7 +55,6 @@ mainfrm = CTkFrame(
 
 passfrm = CTkFrame(
     mainfrm,
-
     #border_width=,
 )
 
@@ -71,11 +69,67 @@ settfrm = CTkFrame(
     #border_color= "#0D1B1E"
 )
 
+images = [
+    PhotoImage(file="images/dark_mode.png"),
+    PhotoImage(file="images/light_mode.png"),
+]
+
+
+
+
+mode_btn = CTkButton(
+    mainfrm,
+    width = 25,
+    height = 25,
+    #fg_color="transparent",
+    image=PhotoImage(file="images/dark_mode.png"),
+    text="",
+    hover=True
+)
+
+
+
+
+def switch_appearance(): #Function to toggle between light and dark mode
+    if get_appearance_mode() == "Dark":  #If current mode is dark
+        set_appearance_mode("light") #Turn on light mode
+        mode_btn.configure(image = images[1]) # Change Picture
+    else:
+        set_appearance_mode("dark") #Turn on light mode
+        mode_btn.configure(image = images[0]) # Change Picture
+
+
+
+mode_btn.configure(command=switch_appearance) #Link toggle button to the function
+
+
 #grid the frames
 fullroot.pack()
-mainfrm.pack(anchor="w",expand=True,fill=BOTH)
-passfrm.grid(row=0,column=0,padx=10,sticky=NSEW,pady=10)
-settfrm.grid(row=0,column=1,padx=10)
+mainfrm.pack(anchor="w",expand=True,fill=BOTH,pady=(10,0))
+
+CTkLabel( #Title Label
+    mainfrm,
+    font=("Arial",35,"bold"),
+    text="Password Generator"
+).grid(
+    columnspan=2,
+    pady=(10,0)
+)
+
+
+mode_btn.grid(
+    row=0,
+    column=1,
+    sticky=E,
+    padx = 20,
+    pady=(10,0)
+)
+
+
+passfrm.grid(row=1,column=0,padx=30,sticky=NSEW,pady=10)
+settfrm.grid(row=1,column=1,padx=10)
+
+
 
 #Intialize Checkbox Dictionary
 characters = {
@@ -125,7 +179,7 @@ def validatecheckboxes():
             if characters[i][2].get(): #If the only enabled checkbox is found
                 characters[i][1].configure(state=DISABLED) #Disable the checkbox
         
-        warninglabel.configure(text="Minimum one checkbox should be enabled",)
+        warninglabel.configure(text="Atleast one checkbox should be enabled",)
         warninglabel.grid(column=0,row=9,sticky="w",padx=10, pady=10) #Display warning label
     
     else:
@@ -140,7 +194,8 @@ for j in characters:
         column=0,
         row=3+list(characters.keys()).index(j),
         sticky="w",
-        padx=10
+        padx=10,
+        pady=5
     )
     
     characters[j][1].configure(
@@ -154,7 +209,8 @@ strengthlabel = CTkLabel( #Label to display the password strength
     text="Password Strength: None",
     font=("Arial",20,"bold"),
     pady=10,
-    padx=50
+    corner_radius=5
+    #padx=50
 )
 
 
@@ -169,29 +225,50 @@ passentry = CTkTextbox( #Label to display password
 
 
 def strengthcheck():
-    temp = passentry.get("1.0","end-1c")
-    passentry.delete("1.0","end")
-    passentry.insert("0.0",temp.replace("\n",""),)
-    pw = passentry.get("1.0","end-1c").strip()
-    print("Checking password ", f"'{pw}'"," using zxcvbn...")
+    temp = passentry.get("1.0","end-1c") #Gets the current text in the textbox
+    passentry.delete("1.0","end") #Clears the textbox
+    passentry.insert("0.0",temp.replace("\n",""),) #Adds the previous text except with no newline characters, preventing newlines to be created each time the user presses Enter
+
+    pw = passentry.get("1.0","end-1c").strip() #Gets the current text from textbox and removes any trailing whitespace
+    
+    print("Checking password ", f"'{pw}'"," using zxcvbn...") 
     result = zxcvbn.zxcvbn(pw) #Put the password through the zxcvbn's zxcvbn function and receive back a dictionary of results
     score = result["score"] #Obtain the score from the dictionary
     
     print("Password Score: ",score)
     print("Number of Guesses: ", result["guesses"])
     print("Time which will be taken to crack: ", result["crack_times_display"]["online_no_throttling_10_per_second"])
+
     strengthlabel.configure(
-        text=f"Password Strength: {strengths[score][0]}",
+        text=f"Password Strength:\n {strengths[score][0]}",
         text_color=strengths[score][1], #Change text color
         fg_color= colour.Color(strengths[score][1],luminance=0.1).hex_l #Utilize colour lib to make a darker version of the strength color
     )
-    
+
+
+
 
 passentry.grid(padx=10,pady=10) #TextBox for Password
+
 passentry.bind("<Return>", lambda event: strengthcheck())
-strengthlabel.grid()
 
+def clipreplace(text:str):
+    root.clipboard_clear() #Clear the Clipboard
+    root.clipboard_append(text) #Add the text to the clipboard
 
+CTkButton( #Create a clipboard button
+    passfrm,
+    width = 25,
+    height = 25,
+    image = PhotoImage(file="images/copy.png"),
+    text = "",
+    hover=True,
+    command=lambda: clipreplace(passentry.get("1.0","end-1c").strip()) #Gets the current text in the password textbox and puts it through the function
+).grid(
+    sticky=SE,pady=(0,10)
+)
+
+strengthlabel.grid(sticky=NSEW,padx=(10,10),ipadx=7)
 
 wordvar = StringVar() #Variable to store entry input
 
@@ -206,38 +283,41 @@ def genpassword():
     print("----------------------------------------------------")
     print("Generating Password!")
     print("Desired Length of Password: ", sliderval.get())
-    start_time = time.time()
-    word = wordvar.get().strip()
-    if len(word) >= sliderval.get():
-        warninglabel.configure(text="Word too long, \nplease enter a shorter word or increase password length",justify="left")
-        warninglabel.grid(column=0,row=9,sticky="w",padx=10, pady=10)
-        return None
-    else:
-        warninglabel.grid_forget()
-    validchars = []
-    for i in characters:
-        if characters[i][2].get():
-            validchars +=characters[i][0]
+    start_time = time.time() #Get time before generation
 
-    pw =""
-    print(validchars)
+    word = wordvar.get().strip() #Gets the word and removes any trailing whitespace
+
+    if len(word) >= sliderval.get(): #Checks if the word is larger than the password length
+        warninglabel.configure(text="Word too long, \nPlease enter a shorter word \nor increase password length",justify="left")
+        warninglabel.grid(column=0,row=9,sticky="w",padx=10, pady=10)
+        return None # return none, ending the function
+    else:
+        warninglabel.grid_forget() # Hide the warning label
     
-    for j in range(int(sliderval.get())-len(word)-1):
+
+    validchars = [] #Creates a empty list,
+    for i in characters:
+        if characters[i][2].get(): #Checks if the corresponding set of characters have their checkbox enabled,
+            validchars +=characters[i][0] #if so, add to the empty list
+
+    pw ="" #Empty string
+    
+    for j in range(int(sliderval.get())-len(word)-1): #Repeats for the length of slidervalue minus the length of the word
         randindex = random.randint(0,len(validchars)-1)
-        print("Random Chosen Index: ",randindex)
-        pw += validchars[randindex]
-        print("Password Progress: ",pw)
+        print("Random Chosen Index: ",randindex) #Chooses a random index in the validchars list
+        pw += validchars[randindex] #Adds the corresponding item to the password
+        print("Password Progress: ",pw) 
 
     if word != "":
-        if bool(random.getrandbits): #Get a random bit (1 or 0) and convert it to a boolean, random boolean
+        if bool(random.getrandbits(1)): #Get a random bit (1 or 0) and convert it to a boolean, random boolean
             print("Attaching Word to: FRONT")
-            pw = word + "-" + pw
+            pw = word + "-" + pw #Adds it to the front of the word with a hyphen seperating it
         else:
             print("Attaching Word to: BACK")
-            pw += '-' + word
+            pw += '-' + word #Adds it to the back of the word with a hyphen seperating it
     
-    end_time = time.time()
-    print("Password generated in: ", (end_time-start_time)*1000, "ms")
+    end_time = time.time() #Gets the time after generation
+    print("Password generated in: ", (end_time-start_time)*1000, "ms") #Prints the total time taken (purely for testing performance)
     passentry.delete("1.0","end-1c") #Clear password textbox
     passentry.insert("0.0",pw) #Insert new password into textbox
     print("Final Password:", pw)
@@ -266,26 +346,22 @@ CTkSlider(
     row=2,
     rowspan=True,
     sticky="nswe",
-    padx=10
+    padx=10,
+    pady=5
 )
-
-
-
 
 CTkButton(
     passfrm, 
     text="Generate", 
     command=genpassword,
+    hover=True
 ).grid(
     column=0,
-    row=2,
     pady=10
 )
 
-
 CTkLabel(settfrm,text="Word to include in password",font=("Arial",20,"bold")).grid(sticky="w",padx=10)
 wordentry.grid(sticky="w",padx=10,pady=10)
-
 
 genpassword()#Generate a password that will be displyed when the program is run
 
